5b3b576ef71092aa8c3b01b71095ce1790b226ad
Walter <wxfwalter@gmail.com>
2016-09-08 21:39:53 +0800

    Replace abbreviations with full words

--- 101_4/twenty_one.rb
+++ 101_4/twenty_one.rb
@@ -16,47 +16,47 @@ def deck_initializer
   deck
 end
 
-def cards_dealer(dck)
+def cards_dealer(deck)
   card_face = ''
   loop do
-    card_face = dck.keys.sample
-    card_value = dck[card_face].pop
+    card_face = deck.keys.sample
+    card_value = deck[card_face].pop
     break if card_value
   end
   card_face
 end
 
-def value_calculator(crd_fce)
-  if crd_fce.include?('Ace')
-    value = CARD.values_at(*crd_fce).inject(:+)
+def value_calculator(card_face)
+  if card_face.include?('Ace')
+    value = CARD.values_at(*card_face).inject(:+)
     value += 10 if value <= 11
   else
-    value = CARD.values_at(*crd_fce).inject(:+)
+    value = CARD.values_at(*card_face).inject(:+)
   end
   value
 end
 
-def bust?(plyer_ttl, cmpter_ttl)
-  if plyer_ttl > 21 && cmpter_ttl > 21
+def bust?(player_total, computer_total)
+  if player_total > 21 && computer_total > 21
     "Both sides have busted, it's a tie"
-  elsif plyer_ttl > 21
+  elsif player_total > 21
     "Player busted. Computer won!"
-  elsif cmpter_ttl > 21
+  elsif computer_total > 21
     "Computer busted. Player won!"
   else
     nil
   end
 end
 
-def display_result(plyer_ttl, cmpter_ttl)
-  if bust?(plyer_ttl, cmpter_ttl)
-    prompt bust?(plyer_ttl, cmpter_ttl)
-  elsif plyer_ttl < cmpter_ttl
-    prompt "Computer won!" + " Computer: #{cmpter_ttl}"
-  elsif plyer_ttl > cmpter_ttl
-    prompt "Player won!" + " Computer: #{cmpter_ttl}"
+def display_result(player_total, computer_total)
+  if bust?(player_total, computer_total)
+    prompt bust?(player_total, computer_total)
+  elsif player_total < computer_total
+    prompt "Computer won! Computer: #{computer_total}"
+  elsif player_total > computer_total
+    prompt "Player won! Computer: #{computer_total}"
   else
-    prompt "It's a tie!" + " Computer: #{cmpter_ttl}"
+    prompt "It's a tie! Computer: #{computer_total}"
   end
 end
 
@@ -72,7 +72,7 @@ loop do
   computer_card.push(cards_dealer(deck))
 
   prompt "Player's cards are: #{player_card.join(', ')}."
-  prompt "One of the computer's card is: #{computer_card.first}."
+  prompt "The dealer is showing #{computer_card.first}."
 
   player_total = value_calculator(player_card)
   computer_total = value_calculator(computer_card)
@@ -80,7 +80,7 @@ loop do
     # player loop
     answer = ''
     loop do
-      prompt "Hit or stay"
+      prompt "Player value is #{player_total}. Hit or stay?"
       answer = gets.chomp.to_s.downcase
       if answer == 'hit'
         player_card.push(cards_dealer(deck))
