04cfaab05b1054a26537ced402e834d51f2c15b4
Walter <wxfwalter@gmail.com>
2016-09-08 18:32:47 +0800

    Main loop

--- 101_4/new_twenty_one.rb
+++ 101_4/new_twenty_one.rb
@@ -1,14 +1,20 @@
 SUITS = ['H', 'D', 'S', 'C']
 VALUES = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
 
-def deck
+def new_deck_shuffler
   cards = []
   VALUES.each do |value|
     SUITS.each do |suit|
       cards.push([suit, value])
     end
   end
-  cards
+  cards.shuffle
 end
 
-cards = deck
+deck = new_deck_shuffler
+
+player_cards = []
+player_cards << deck.pop
+player_cards << deck.pop
+
+p player_cards
