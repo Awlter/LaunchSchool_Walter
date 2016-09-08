d244a9a7c8f4c3228af3775e54db64dea2ccff51
Walter <wxfwalter@gmail.com>
2016-09-08 15:05:10 +0800

    new: initializer

---
+++ 101_4/new_twenty_one.rb
@@ -0,0 +1,15 @@
+SUITS = ['H', 'D', 'S', 'C']
+VALUES = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+
+def deck_initializer
+  cards = []
+  VALUES.each do |value|
+    SUITS.each do |suit|
+      cards.push([suit, value])
+    end
+  end
+  cards
+end
+
+cards = deck_initializer
+p cards
