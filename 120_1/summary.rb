f06e6d95c11becf116f4be4ff1b32c7af0a1873c
Walter <wxfwalter@gmail.com>
2016-10-10 11:06:07 +0800

    Recall oop book

---
+++ 120_1/summary.rb
@@ -0,0 +1,55 @@
+Recall OOP
+
+### version 1
+
+- OOP
+object oriented programming is a paradigm different from procedural and function programming.
+
+- Ruby
+Ruby is a pure oop language. Everything in Ruby is an object.
+
+- class is the blueprint of an object, states, behaviors
+
+- object
+
+- object variables
+
+- class variables
+
+- instance method
+
+- constructor -- initialize
+
+- getters and setters
+
+- attr_accessor, self
+
+- class methods, self
+
+- module, include, extend
+
+- inheritance
+
+- super
+
+- private, protected
+
+- to_s methods
+
+- puts, #{}
+
+### version 2 (after rereading the oop textbook)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
