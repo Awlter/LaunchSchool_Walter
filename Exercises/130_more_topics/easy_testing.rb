545a5e68c508a55ea1ff430fc0e5a46d673c923e
Walter <wxfwalter@gmail.com>
2016-10-28 11:12:24 +0800

    Exercise 130 easy_testing

--- Exercises/130_more_topics/easy_testing.rb
+++ Exercises/130_more_topics/easy_testing.rb
@@ -1,3 +1,12 @@
-value = 2
+# value = 2
 
-assert value.odd?, 'value is not odd'
+# assert value.odd?, 'value is not odd'
+
+# assert_equal 'XYZ' value.downcase
+
+#
+# assert_nil(true)
+
+# assert_raises NoExperienceError do
+#   employee.hire
+# end
