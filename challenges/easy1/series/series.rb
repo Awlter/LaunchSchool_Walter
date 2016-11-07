e8ee74d1b07033258fc5ace05c57b2cd562bec82
Walter <wxfwalter@gmail.com>
2016-11-07 14:45:28 +0800

    challenge easy 1 series

--- challenges/easy1/series/series.rb
+++ challenges/easy1/series/series.rb
@@ -1,26 +1,26 @@
 # my version
 # class Series
 #   def initialize(digits)
-#     @digits = digits
+#     @digits = digits.chars.map(&:to_i)
 #   end
 
-#   def consecutive_of_n(input: $stdin)
-#     puts "Input a number:"
-#     n = input.gets.chomp.to_i
-
+#   def slices(n)
+#     raise ArgumentError, "Blow up" if n > @digits.size
 #     result = (0..@digits.size - n).map do |indx|
 #       @digits.slice(indx, n)
 #     end
 #   end
 # end
 
-class Series
-  def initialize(number)
-    @number = number.each_char.map(&:to_i)
-  end
+# others' version
 
-  def slices(n)
-    raise ArgumentError, "Blow up" if n > @number.size
-    @number.each_cons(n).to_a
-  end
-end
+# class Series
+#   def initialize(number)
+#     @number = number.chars.map(&:to_i)
+#   end
+
+#   def slices(n)
+#     raise ArgumentError, "Blow up" if n > @number.size
+#     @number.each_cons(n).to_a
+#   end
+# end
