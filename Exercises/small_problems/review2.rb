283d1ecc325683546f6472d36e4d6449676e2075
Walter <wxfwalter@gmail.com>
2016-11-09 10:15:06 +0800

    review exercise small problem

--- Exercises/small_problems/review2.rb
+++ Exercises/small_problems/review2.rb
@@ -238,7 +238,7 @@ DIGIT_HASH = {
 # def word_to_digit(string)
 #   result = ''
 
-#   words_with_spaces = string.split(/\b/)
+#   words_with_indents = string.split(/\b/)
 
 #   words_with_spaces.each do |element|
 #     number = DIGIT_ARRAY.index(element).to_s
@@ -584,175 +584,520 @@ end
 #   end
 # end
 
-class PokerHand
-  def initialize(deck)
-    @cards = []
-    @rank_count = Hash.new(0)
+# class PokerHand
+#   def initialize(deck)
+#     @cards = []
+#     @rank_count = Hash.new(0)
 
-    5.times do
-      card = deck.draw
-      @cards << card
-      @rank_count[card.rank] += 1
-    end
-  end
+#     5.times do
+#       card = deck.draw
+#       @cards << card
+#       @rank_count[card.rank] += 1
+#     end
+#   end
 
-  def print
-    puts @cards
-  end
+#   def print
+#     puts @cards
+#   end
 
-  def evaluate
-    case
-    when royal_flush?     then 'Royal flush'
-    when straight_flush?  then 'Straight flush'
-    when four_of_a_kind?  then 'Four of a kind'
-    when full_house?      then 'Full house'
-    when flush?           then 'Flush'
-    when straight?        then 'Straight'
-    when three_of_a_kind? then 'Three of a kind'
-    when two_pair?        then 'Two pair'
-    when pair?            then 'Pair'
-    else                       'High card'
-    end
-  end
+#   def evaluate
+#     case
+#     when royal_flush?     then 'Royal flush'
+#     when straight_flush?  then 'Straight flush'
+#     when four_of_a_kind?  then 'Four of a kind'
+#     when full_house?      then 'Full house'
+#     when flush?           then 'Flush'
+#     when straight?        then 'Straight'
+#     when three_of_a_kind? then 'Three of a kind'
+#     when two_pair?        then 'Two pair'
+#     when pair?            then 'Pair'
+#     else                       'High card'
+#     end
+#   end
 
-  private
+#   private
 
-  def royal_flush?
-    straight_flush? && @cards.min.value == 10
-  end
+#   def royal_flush?
+#     straight_flush? && @cards.min.value == 10
+#   end
 
-  def straight_flush?
-    straight? && flush?
-  end
+#   def straight_flush?
+#     straight? && flush?
+#   end
 
-  def four_of_a_kind?
-    n_of_a_kind?(4)
-  end
+#   def four_of_a_kind?
+#     n_of_a_kind?(4)
+#   end
 
-  def full_house?
-    n_of_a_kind?(3) && n_of_a_kind?(2)
-  end
+#   def full_house?
+#     n_of_a_kind?(3) && n_of_a_kind?(2)
+#   end
 
-  def flush?
-    suit = @cards.first.suit
-    @cards.all? { |card| card.suit == suit }
-  end
+#   def flush?
+#     suit = @cards.first.suit
+#     @cards.all? { |card| card.suit == suit }
+#   end
 
-  def straight?
-    return false if @rank_count.any? { |_, count| count > 1 }
+#   def straight?
+#     return false if @rank_count.any? { |_, count| count > 1 }
     
-    @cards.min.value == @cards.max.value - 4 
+#     @cards.min.value == @cards.max.value - 4 
+#   end
+
+#   def n_of_a_kind?(number)
+#     @rank_count.one? { |_, count| count == number }
+#   end
+
+#   def three_of_a_kind?
+#     n_of_a_kind?(3)
+#   end
+
+#   def two_pair?
+#     @rank_count.select { |_, count| count == 2 }.size == 2
+#   end
+
+#   def pair?
+#     n_of_a_kind?(2)
+#   end
+# end
+
+# # Danger danger danger: monkey
+# # patching for testing purposes.
+# class Array
+#   alias_method :draw, :pop
+# end
+
+# hand = PokerHand.new([
+#   Card.new(10,      'Hearts'),
+#   Card.new('Ace',   'Hearts'),
+#   Card.new('Queen', 'Hearts'),
+#   Card.new('King',  'Hearts'),
+#   Card.new('Jack',  'Hearts')
+# ])
+# puts hand.evaluate == 'Royal flush'
+
+# hand = PokerHand.new([
+#   Card.new(8,       'Clubs'),
+#   Card.new(9,       'Clubs'),
+#   Card.new('Queen', 'Clubs'),
+#   Card.new(10,      'Clubs'),
+#   Card.new('Jack',  'Clubs')
+# ])
+# puts hand.evaluate == 'Straight flush'
+
+# hand = PokerHand.new([
+#   Card.new(3, 'Hearts'),
+#   Card.new(3, 'Clubs'),
+#   Card.new(5, 'Diamonds'),
+#   Card.new(3, 'Spades'),
+#   Card.new(3, 'Diamonds')
+# ])
+# puts hand.evaluate == 'Four of a kind'
+
+# hand = PokerHand.new([
+#   Card.new(3, 'Hearts'),
+#   Card.new(3, 'Clubs'),
+#   Card.new(5, 'Diamonds'),
+#   Card.new(3, 'Spades'),
+#   Card.new(5, 'Hearts')
+# ])
+# puts hand.evaluate == 'Full house'
+
+# hand = PokerHand.new([
+#   Card.new(10, 'Hearts'),
+#   Card.new('Ace', 'Hearts'),
+#   Card.new(2, 'Hearts'),
+#   Card.new('King', 'Hearts'),
+#   Card.new(3, 'Hearts')
+# ])
+# puts hand.evaluate == 'Flush'
+
+# hand = PokerHand.new([
+#   Card.new(8,      'Clubs'),
+#   Card.new(9,      'Diamonds'),
+#   Card.new(10,     'Clubs'),
+#   Card.new(7,      'Hearts'),
+#   Card.new('Jack', 'Clubs')
+# ])
+# puts hand.evaluate == 'Straight'
+
+# hand = PokerHand.new([
+#   Card.new(3, 'Hearts'),
+#   Card.new(3, 'Clubs'),
+#   Card.new(5, 'Diamonds'),
+#   Card.new(3, 'Spades'),
+#   Card.new(6, 'Diamonds')
+# ])
+# puts hand.evaluate == 'Three of a kind'
+
+# hand = PokerHand.new([
+#   Card.new(9, 'Hearts'),
+#   Card.new(9, 'Clubs'),
+#   Card.new(5, 'Diamonds'),
+#   Card.new(8, 'Spades'),
+#   Card.new(5, 'Hearts')
+# ])
+# puts hand.evaluate == 'Two pair'
+
+# hand = PokerHand.new([
+#   Card.new(2, 'Hearts'),
+#   Card.new(9, 'Clubs'),
+#   Card.new(5, 'Diamonds'),
+#   Card.new(9, 'Spades'),
+#   Card.new(3, 'Diamonds')
+# ])
+# puts hand.evaluate == 'Pair'
+
+# hand = PokerHand.new([
+#   Card.new(2,      'Hearts'),
+#   Card.new('King', 'Clubs'),
+#   Card.new(5,      'Diamonds'),
+#   Card.new(9,      'Spades'),
+#   Card.new(3,      'Diamonds')
+# ])
+# puts hand.evaluate == 'High card'
+
+# def letter_percentages(string)
+#   length = string.length
+#   result = {}
+#   result[:lowercase] = string.count('a-z').to_f * 100 / length
+#   result[:uppercase] = string.count('A-Z').to_f * 100 / length
+#   result[:neither] = string.count('^a-zA-Z').to_f * 100 / length
+#   result
+# end
+
+# p letter_percentages('abCdef 123') == { lowercase: 50, uppercase: 10, neither: 40 }
+# p letter_percentages('AbCd +Ef') == { lowercase: 37.5, uppercase: 37.5, neither: 25 }
+# p letter_percentages('123') == { lowercase: 0, uppercase: 0, neither: 100 }
+
+# def balanced?(string)
+#   return false if string.count('(') != string.count(')')
+
+#   count = 0
+#   string.scan(/\(|\)/).each do |paren|
+#     case paren
+#     when '(' then count += 1
+#     when ')' then count -= 1
+#     end
+#     return false if count < 0
+#   end
+
+#   true
+# end
+
+# p balanced?('What (is) this?') == true
+# p balanced?('What is) this?') == false
+# p balanced?('What (is this?') == false
+# p balanced?('((What) (is this))?') == true
+# p balanced?('((What)) (is this))?') == false
+# p balanced?('Hey!') == true
+# p balanced?(')Hey!(') == false
+# p balanced?('What ((is))) up(') == false
+
+# def triangle(*sides)
+#   sides.sort!
+#   if sides[0] + sides[1] <= sides[2]
+#     :invalid
+#   elsif sides[0] == sides[2]
+#     :equilateral
+#   elsif sides[1] == sides[2]
+#     :isosceles
+#   else
+#     :scalene
+#   end
+# end
+
+# p triangle(3, 3, 3) == :equilateral
+# p triangle(3, 3, 1.5) == :isosceles
+# p triangle(3, 4, 5) == :scalene
+# p triangle(0, 3, 3) == :invalid
+# p triangle(3, 1, 1) == :invalid
+
+# def friday_13th?(year)
+#   result = 0
+
+#   (1..12).each do |month|
+#     time = Time.new(year, month, 13)
+#     result += 1 if time.friday?
+#   end
+
+#   result
+# end
+
+# p friday_13th?(2015) == 3
+# p friday_13th?(1986) == 1
+
+# def featured(number)
+#   loop do
+#     number += 1
+#     break if number.odd? && number % 7 == 0
+#   end
+
+#   loop do
+#     numbers = number.to_s.chars
+#     break if numbers == numbers.uniq
+#     number += 14
+#   end
+
+#   number
+# end
+
+# p featured(12) == 21
+# p featured(20) == 21
+# p featured(21) == 35
+# p featured(997) == 1029
+# p featured(1029) == 1043
+# p featured(999_999) == 1_023_547
+# p featured(999_999_987) == 1_023_456_987
+
+# p featured(9_999_999_999) # -> There is no possible number that fulfills those require
+
+# def bubble_sort!(array)
+#   loop do
+#     swapped = false
+#     (1...array.size).each do |indx|
+#       next if array[indx - 1] <= array[indx]
+#       array[indx - 1], array[indx] = array[indx], array[indx - 1]
+#       swapped = true    
+#     end
+
+#     break unless swapped
+#   end
+# end
+
+# array = %w(Sue Pete Alice Tyler Rachel Kim Bonnie)
+# bubble_sort!(array)
+# p array == %w(Alice Bonnie Kim Pete Rachel Sue Tyler)
+
+# def sum_square_difference(number)
+#   sum_square = (1..number).to_a.reduce(:+) ** 2
+
+#   square_sum = (1..number).to_a.map { |n| n**2 }.reduce(:+)
+
+#   sum_square - square_sum
+# end
+
+# p sum_square_difference(3) == 22
+#    # -> (1 + 2 + 3)**2 - (1**2 + 2**2 + 3**2)
+# p sum_square_difference(10) == 2640
+# p sum_square_difference(1) == 0
+# p sum_square_difference(100) == 25164150
+
+# def madlibs
+#   noun = %w(na nb nc nd).sample
+#   verb = %w(va vb vc vd).sample
+#   adjective = %w(aa ab ac ad).sample
+#   adverb = %w(ava avb avc avd).sample
+
+#   File.open('simple_file.txt', 'r').each do |line|
+#     puts format(line, { noun: noun, adjective: adjective, adverb: adverb, verb: verb} )
+#   end
+# end
+
+# madlibs
+
+# def star(size)
+#   indent_numbers = (0...size/2).to_a
+
+#   indent_numbers.reverse.each do |number|
+#     puts "*#{' '*number}*#{' '*number}*".center(size)
+#   end
+
+#   puts "*" * size
+
+#   indent_numbers.each do |number|
+#     puts "*#{' '*number}*#{' '*number}*".center(size)
+#   end
+# end
+
+# star(9)
+
+# def bubble_sort!(array)
+#   last_number = array.size - 1
+  
+#   loop do
+#     swapped_times = 0 
+#     1.upto(last_number) do |indx|
+#       next if array[indx - 1] <= array[indx]
+#       array[indx - 1], array[indx] = array[indx], array[indx - 1]
+#       swapped_times += 1
+#     end
+
+#     break if swapped_times == 1
+#   end
+# end
+
+# array = [5, 3]
+# bubble_sort!(array)
+# p array == [3, 5]
+
+# array = [6, 2, 7, 1, 4]
+# bubble_sort!(array)
+# p array == [1, 2, 4, 6, 7]
+
+# def transpose(matrix)
+#   result = Array.new(3) { Array.new }
+
+#   matrix.each do |sub_array|
+#     sub_array.each_with_index do |num, indx|
+#       result[indx] << num
+#     end
+#   end
+
+#   result
+# end
+
+# def transpose!(matrix)
+#   (0...matrix.size - 1).each do |indx|
+#     (indx + 1...matrix.size).each do |indx1|
+#       matrix[indx][indx1], matrix[indx1][indx] = matrix[indx1][indx], matrix[indx][indx1]
+#     end
+#   end
+#   matrix
+# end
+
+# matrix = [
+#   [1, 2, 3, 4],
+#   [5, 6, 7, 8],
+#   [9, 0, 1, 2],
+#   [3, 4, 5, 6]
+# ]
+
+# new_matrix = transpose!(matrix)
+
+# p new_matrix# == [[1, 4, 3], [5, 7, 9], [8, 2, 6]]
+# p matrix == [[1, 4, 3], [5, 7, 9], [8, 2, 6]]
+
+# def transpose(matrix)
+#   result = Array.new(matrix.first.size) { Array.new }
+
+#   matrix.each do |sub_array|
+#     sub_array.each_with_index do |num, indx|
+#       result[indx] << num
+#     end
+#   end
+
+#   result
+# end
+
+# def transpose(matrix)
+#   result = []
+
+#   (0...matrix.first.size).each do |indx|
+#     sub_result = matrix.map { |sub_array| sub_array[indx] }
+#     result << sub_result
+#   end
+
+#   result
+# end
+
+# def rotate90(matrix)
+#   matrix = matrix.reverse
+#   result = []
+
+#   number_of_rows = matrix.first.size
+#   number_of_columns = matrix.size
+
+#   (0...number_of_rows).each do |row_indx|
+#     new_row = (0...number_of_columns).map { |column_indx| matrix[column_indx][row_indx] }
+#     result << new_row
+#   end
+
+#   result
+# end
+
+# p transpose([[1, 2, 3, 4]]) == [[1], [2], [3], [4]]
+# p transpose([[1], [2], [3], [4]]) == [[1, 2, 3, 4]]
+# p transpose([[1, 2, 3, 4, 5], [4, 3, 2, 1, 0], [3, 7, 8, 6, 2]]) ==
+#   [[1, 4, 3], [2, 3, 7], [3, 2, 8], [4, 1, 6], [5, 0, 2]]
+# p transpose([[1]]) == [[1]]
+
+# # def rotate90(matrix)
+#   result = []
+
+#   number_of_columns = matrix.first.size
+#   number_of_rows = matrix.size
+
+#   (0...number_of_columns).each do |column_indx|
+#     new_row = (0...number_of_rows).to_a.reverse.map { |row_indx| matrix[row_indx][column_indx]}
+#     result << new_row
+#   end
+
+#   result
+# end
+
+
+# matrix1 = [
+#   [1, 5, 8],
+#   [4, 7, 2],
+#   [3, 9, 6]
+# ]
+
+# matrix2 = [
+#   [3, 7, 4, 2],
+#   [5, 1, 0, 8]
+# ]
+
+# new_matrix1 = rotate90(matrix1)
+# new_matrix2 = rotate90(matrix2)
+# new_matrix3 = rotate90(rotate90(rotate90(rotate90(matrix2))))
+
+# p new_matrix1 == [[3, 4, 1], [9, 7, 5], [6, 2, 8]]
+# p new_matrix2 == [[5, 3], [1, 7], [0, 4], [8, 2]]
+# p new_matrix3 == matrix2
+
+def permutations(array)
+  result = array.combination(1).to_a
+
+  loop do
+    sub_result = []
+    array.each do |num|
+      sub_result << result.map do |sub_array|
+        sub_array.unshift(num) unless sub_array.include? num
+      end
+    end
+
+    result = sub_result
+
+    break if result.first.size == array.size
   end
 
-  def n_of_a_kind?(number)
-    @rank_count.one? { |_, count| count == number }
-  end
-
-  def three_of_a_kind?
-    n_of_a_kind?(3)
-  end
-
-  def two_pair?
-    @rank_count.select { |_, count| count == 2 }.size == 2
-  end
-
-  def pair?
-    n_of_a_kind?(2)
-  end
+  result
 end
 
-# Danger danger danger: monkey
-# patching for testing purposes.
-class Array
-  alias_method :draw, :pop
-end
+p permutations([2])
+# -> [[2]]
 
-hand = PokerHand.new([
-  Card.new(10,      'Hearts'),
-  Card.new('Ace',   'Hearts'),
-  Card.new('Queen', 'Hearts'),
-  Card.new('King',  'Hearts'),
-  Card.new('Jack',  'Hearts')
-])
-puts hand.evaluate == 'Royal flush'
+p permutations([1, 2])
+# -> [[1, 2], [2, 1]]
 
-hand = PokerHand.new([
-  Card.new(8,       'Clubs'),
-  Card.new(9,       'Clubs'),
-  Card.new('Queen', 'Clubs'),
-  Card.new(10,      'Clubs'),
-  Card.new('Jack',  'Clubs')
-])
-puts hand.evaluate == 'Straight flush'
+p permutations([1, 2, 3])
+# # -> [[1, 2, 3], [1, 3, 2], [2, 1, 3], [2, 3, 1], [3, 1, 2], [3, 2, 1]]
 
-hand = PokerHand.new([
-  Card.new(3, 'Hearts'),
-  Card.new(3, 'Clubs'),
-  Card.new(5, 'Diamonds'),
-  Card.new(3, 'Spades'),
-  Card.new(3, 'Diamonds')
-])
-puts hand.evaluate == 'Four of a kind'
+# p permutations([1, 2, 3, 4])
+# # -> [[1, 2, 3, 4], [1, 2, 4, 3], [1, 3, 2, 4],
+#     [1, 3, 4, 2], [1, 4, 2, 3], [1, 4, 3, 2],
+#     [2, 1, 3, 4], [2, 1, 4, 3], [2, 3, 1, 4],
+#     [2, 3, 4, 1], [2, 4, 1, 3], [2, 4, 3, 1],
+#     [3, 1, 2, 4], [3, 1, 4, 2], [3, 2, 1, 4],
+#     [3, 2, 4, 1], [3, 4, 1, 2], [3, 4, 2, 1],
+#     [4, 1, 2, 3], [4, 1, 3, 2], [4, 2, 1, 3],
+#     [4, 2, 3, 1], [4, 3, 1, 2], [4, 3, 2, 1]]
 
-hand = PokerHand.new([
-  Card.new(3, 'Hearts'),
-  Card.new(3, 'Clubs'),
-  Card.new(5, 'Diamonds'),
-  Card.new(3, 'Spades'),
-  Card.new(5, 'Hearts')
-])
-puts hand.evaluate == 'Full house'
 
-hand = PokerHand.new([
-  Card.new(10, 'Hearts'),
-  Card.new('Ace', 'Hearts'),
-  Card.new(2, 'Hearts'),
-  Card.new('King', 'Hearts'),
-  Card.new(3, 'Hearts')
-])
-puts hand.evaluate == 'Flush'
 
-hand = PokerHand.new([
-  Card.new(8,      'Clubs'),
-  Card.new(9,      'Diamonds'),
-  Card.new(10,     'Clubs'),
-  Card.new(7,      'Hearts'),
-  Card.new('Jack', 'Clubs')
-])
-puts hand.evaluate == 'Straight'
 
-hand = PokerHand.new([
-  Card.new(3, 'Hearts'),
-  Card.new(3, 'Clubs'),
-  Card.new(5, 'Diamonds'),
-  Card.new(3, 'Spades'),
-  Card.new(6, 'Diamonds')
-])
-puts hand.evaluate == 'Three of a kind'
 
-hand = PokerHand.new([
-  Card.new(9, 'Hearts'),
-  Card.new(9, 'Clubs'),
-  Card.new(5, 'Diamonds'),
-  Card.new(8, 'Spades'),
-  Card.new(5, 'Hearts')
-])
-puts hand.evaluate == 'Two pair'
 
-hand = PokerHand.new([
-  Card.new(2, 'Hearts'),
-  Card.new(9, 'Clubs'),
-  Card.new(5, 'Diamonds'),
-  Card.new(9, 'Spades'),
-  Card.new(3, 'Diamonds')
-])
-puts hand.evaluate == 'Pair'
 
-hand = PokerHand.new([
-  Card.new(2,      'Hearts'),
-  Card.new('King', 'Clubs'),
-  Card.new(5,      'Diamonds'),
-  Card.new(9,      'Spades'),
-  Card.new(3,      'Diamonds')
-])
-puts hand.evaluate == 'High card'
+
+
+
+
+
+
+
+
+
+
+
