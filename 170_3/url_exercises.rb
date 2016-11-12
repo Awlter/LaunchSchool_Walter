ffd8e1dc5b09786b365d6042cbad5797763b6f13
Walter <wxfwalter@gmail.com>
2016-11-12 21:42:06 +0800

    170_4 chapter.erb fail to render

---
+++ 170_3/url_exercises.rb
@@ -0,0 +1,10 @@
+Given the following URL:
+
+https://amazon.com/Double-Stainless-Commercial-Refrigerator/B60HON32?ie=UTF8&qid=142952676&sr=93&keywords=commercial+fridge
+
+a. Identify the **host**  amazon.com
+b. Identify the **names** of the **query parameters**Show Solution ie qid sr keywords
+c. Identify the **values** of the **query parameters**Show Solution
+d. Identify the **protocol**Show Solution https
+e. Identify the **path**Show Solution /Double-Stainless-Commercial-Refrigerator/B60HON32
+f. Identify the **port**S
--- playgaround.rb
+++ playgaround.rb
@@ -1,91 +1,13 @@
-class Pet
-  attr_reader :species, :name
-
-  def initialize(species, name)
-    @species = species
-    @name = name
-  end
-
-  def to_s
-    "a #{species} named #{name}."
-  end
+def find_nb(m)
+    sum = 0
+    
+    result = (1..Float::INFINITY).each do |num|
+      sum += num ** 3
+      break(num) if sum == m
+      break(-1) if sum > 999999999999999
+    end
+    
+    result
 end
 
-class Owner
-  attr_reader :name, :pets
-
-  def initialize(name)
-    @name = name
-    @pets = []
-  end
-
-  def add(pet)
-    @pets << pet
-  end
-
-  def number_of_pets
-    @pets.size
-  end
-
-  def print_pets
-    pets.each do |pet|
-      puts pet
-    end
-  end
-end
-
-class Shelter
-  def initialize
-    @owners = []
-    @pets = []
-  end
-
-  def adopt(owner, pet)
-    owner.add(pet)
-    @owners << owner
-  end
-
-  def add(pet)
-    @pets << pet
-  end
-
-  def print_unadoptions
-    puts "The Animal Shelter has the following unadopted pets:"
-    @pets.each do |pet|
-      puts pet
-    end
-    puts ''
-  end
-
-  def print_adoptions
-    @owners.uniq.each do |owner|
-      puts "#{owner.name} has adopted the following pets"
-      owner.print_pets
-      puts ''
-    end
-  end
-end
-
-butterscotch = Pet.new('cat', 'Butterscotch')
-pudding      = Pet.new('cat', 'Pudding')
-darwin       = Pet.new('bearded dragon', 'Darwin')
-kennedy      = Pet.new('dog', 'Kennedy')
-sweetie      = Pet.new('parakeet', 'Sweetie Pie')
-molly        = Pet.new('dog', 'Molly')
-chester      = Pet.new('fish', 'Chester')
-
-phanson = Owner.new('P Hanson')
-bholmes = Owner.new('B Holmes')
-
-shelter = Shelter.new
-shelter.adopt(phanson, butterscotch)
-shelter.add(pudding)
-shelter.add(darwin)
-shelter.adopt(bholmes, kennedy)
-shelter.adopt(bholmes, sweetie)
-shelter.add(molly)
-shelter.add(chester)
-shelter.print_adoptions
-shelter.print_unadoptions
-puts "#{phanson.name} has #{phanson.number_of_pets} adopted pets."
-puts "#{bholmes.name} has #{bholmes.number_of_pets} adopted pets."
+p find_nb(24723578342962)
