944beab05f541a2041a968e02daf88bc0101f450
Walter <wxfwalter@gmail.com>
2016-12-11 23:24:23 +0800

    Finish sieve

--- challenges/easy1/2sieves/re_sieves.rb
+++ challenges/easy1/2sieves/re_sieves.rb
@@ -1,24 +1,43 @@
 require 'pry'
 
-class Sieve
-  def initialize(limit)
-    @limit = limit
-    @numbers = {}
-    (2..limit).each do |num|
-      @numbers[num] = true
-    end
-  end
+# class Sieve
+#   def initialize(limit)
+#     @limit = limit
+#     @numbers = {}
+#     (2..limit).each do |num|
+#       @numbers[num] = true
+#     end
+#   end
 
-  def primes
-    @numbers.each do |number, state|
-      (number * 2..@limit).step(number) do |num|
-        @numbers[num] = false
-      end 
-    end
+#   def primes
+#     @numbers.each do |number, state|
+#       (number * 2..@limit).step(number) do |num|
+#         @numbers[num] = false
+#       end 
+#     end
 
-    @numbers.select { |number, state| state }.keys
-  end
-end
+#     @numbers.select { |number, state| state }.keys
+#   end
+# end
+
+# class Sieve
+#   def initialize(limit)
+#     @limit = limit
+#   end
+
+#   def primes
+#     prime_numbers = []
+#     range = (2..@limit).to_a
+
+#     until range.empty?
+#       next_prime = range.shift
+#       prime_numbers << next_prime
+#       range.reject! { |num| num % next_prime == 0 }
+#     end
+
+#     prime_numbers
+#   end
+# end
 
 # - all the primes from 2 up to a given number.
 
