ab7f6760852ea9829bc96224d40c5939595a2abc
Walter <wxfwalter@gmail.com>
2017-02-12 10:08:24 +0800

    Finish 210_3 logic_flow

--- challenges/easy1/8word_count/word_count.rb
+++ challenges/easy1/8word_count/word_count.rb
@@ -6,7 +6,7 @@ class Phrase
   def word_count
     hash = Hash.new(0)
 
-    @sentence.scan(/\w+'?\w+|\w+/).each do |word|
+    @sentence.scan(/\b[a-zA-Z0-9_']+\b/).each do |word|
       hash[word.downcase] += 1
     end
 
--- challenges/easy1/9perfect_number/test_perfect_numbers.rb
+++ challenges/easy1/9perfect_number/test_perfect_numbers.rb
@@ -1,12 +1,12 @@
 require 'minitest/autorun'
-require_relative 'perfect_numbers'
+require_relative 're_perfect_number'
 
 class PerfectNumberTest < Minitest::Test
-  # def test_initialize_perfect_number
-  #   assert_raises RuntimeError do
-  #     PerfectNumber.classify(-1)
-  #   end
-  # end
+  def test_initialize_perfect_number
+    assert_raises RuntimeError do
+      PerfectNumber.classify(-1)
+    end
+  end
 
   def test_classify_deficient
     assert_equal 'deficient', PerfectNumber.classify(13)
