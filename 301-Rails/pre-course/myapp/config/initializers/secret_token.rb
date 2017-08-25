ca190a02a85902613f394784de342214a272c1a2
Walter <wxfwalter@gmail.com>
2017-08-25 15:21:05 +0800

    finish rails precourse

---
+++ 301-Rails/pre-course/myapp/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Myapp::Application.config.secret_key_base = 'b5ab5f329228f2b0ba0bff09c2a9aac37ee926f2ddf322b9ebc15f70ca5057ba1ca4f6eb2c648d00561f0a31a37add2b0b67315d616e5184e6a877aeb845921b'
